2beabd204a44c459a8231a81eba131b6017ddd59
Darryl Lardizabal <d.lardizabal@celonis.com>
2024-02-02 01:25:39 -0800

    Added base level of interactivity for chatting.

--- src/app.py
+++ src/app.py
@@ -7,3 +7,12 @@ st.title("Chat with Celonis")
 with st.sidebar:
     st.header("Settings")
     web_url = st.text_input("Website URL")
+
+user_query = st.chat_input("Type your message here...")
+if user_query is not None and user_query != "":
+
+    with st.chat_message("Human"):
+        st.write(user_query)
+
+    with st.chat_message("AI"):
+        st.write("I don't know")
