e14df40f042e310812ed7aef39ed3a11a5cbcedc
Darryl Lardizabal <d.lardizabal@celonis.com>
2024-02-02 01:39:48 -0800

    Took out debugging in sidebar and added it into chat window instead of better UI experience.

--- src/app.py
+++ src/app.py
@@ -7,9 +7,11 @@ def get_response(user_input):
 #app config
 st.set_page_config(page_title="Chat with Celonis", page_icon="💬")
 st.title("Chat with Celonis")
-chat_history = [
-    AIMessage(content="Hello, I'm a JR. Solution Architect. How can I help you?"),
-]
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Hello, I'm a JR. Solution Architect. How can I help you?"),
+    ]
 
 #sidebar
 with st.sidebar:
@@ -20,8 +22,14 @@ with st.sidebar:
 user_query = st.chat_input("Type your message here...")
 if user_query is not None and user_query != "":
     response = get_response(user_query)
-    chat_history.append(HumanMessage(content=user_query))
-    chat_history.append(AIMessage(content=response))
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+    st.session_state.chat_history.append(AIMessage(content=response))
 
-with st.sidebar:
-    st.write(chat_history)
+#conversation
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.write(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.write(message.content)
